e742d69e380193b6888ab722458c112514f0798c
Kris Foster <kfoster@salesforce.com>
2018-05-22 15:31:31 +0100

    needs manual test

--- tests/utilities.js
+++ tests/utilities.js
@@ -3,14 +3,14 @@ const u = require('../utilities.js');
 const { easyJSON, complicatedJSON } = require('./utils');
 
 describe('Escaping and Stringifying Objects: ', () => {
-  it('Ok, easy JSON stringified', (done) => {
+  it('Ok, easy JSON stringified and parsed again', (done) => {
     const stringified = u.escapeAndStringify(easyJSON);
     const reverted = u.parseAndUnescape(stringified);
     sinon.assert.match(reverted, easyJSON);
     done();
   });
 
-  it('Ok, complicated JSON stringified', (done) => {
+  it('Ok, complicated JSON stringified and parsed again', (done) => {
     const stringified = u.escapeAndStringify(complicatedJSON);
     const reverted = u.parseAndUnescape(stringified);
     sinon.assert.match(reverted, complicatedJSON);
